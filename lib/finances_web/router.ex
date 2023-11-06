9821062b7252ac531d0585853530dfce93af114c
Will Amaral <will@amaral.dev>
2023-11-06 12:51:07 -0300

    fix: adjust layouts

--- lib/finances_web/router.ex
+++ lib/finances_web/router.ex
@@ -17,24 +17,8 @@ defmodule FinancesWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", FinancesWeb do
-    pipe_through :browser
-
-    get "/", PageController, :home
-  end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", FinancesWeb do
-  #   pipe_through :api
-  # end
-
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:finances, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
@@ -63,6 +47,8 @@ defmodule FinancesWeb.Router do
 
   scope "/", FinancesWeb do
     pipe_through [:browser, :require_authenticated_user]
+
+    get "/", PageController, :home
 
     live_session :require_authenticated_user,
       on_mount: [{FinancesWeb.UserAuth, :ensure_authenticated}] do
