039304c85c2d35eea515f76e1608d1688e06cf47
Will Amaral <will@amaral.dev>
2023-11-06 20:56:15 -0300

    test: fix broken tests

--- test/finances_web/controllers/page_controller_test.exs
+++ test/finances_web/controllers/page_controller_test.exs
@@ -1,8 +1,14 @@
 defmodule FinancesWeb.PageControllerTest do
   use FinancesWeb.ConnCase
 
+  import Finances.AccountsFixtures
+
   test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
+    conn =
+      conn
+      |> log_in_user(user_fixture())
+      |> get(~p"/")
+
     assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
   end
 end
