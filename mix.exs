e421c05f1b2dff232ca815b2ac2152f126cafd21
Will Amaral <will@amaral.dev>
2023-11-06 21:07:40 -0300

    chore: add coverage reporter

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule Finances.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:excoveralls, "~> 0.18", only: :test}
     ]
   end
 
